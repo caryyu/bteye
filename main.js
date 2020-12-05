d8e0470b6461e9a7da78a96853f0846ebfed89e5
caryyu <cary.yu@wework.com>
2020-12-05 12:29:22 +0800

    modernize the whole script

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         btcloud
+// @name         btdouban
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  [!!!Ensure your network is under VPN] Gets Douban medias' BitTorrent resources
+// @author       shadow-walker
 // @match        http://*/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_openInTab
@@ -12,39 +12,127 @@
 // @include        *//movie.douban.com/subject/*
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
-	var movieTitle = $('h1 span:eq(0)').text()
-	var title = $('html head title').text()
-	var keyword1 = title.replace('(豆瓣)', '').trim()
-	var keyword2 = encodeURIComponent( keyword1 )
-	var keyword3 = encodeURIComponent( keyword2 )
-	var MovieOriginalTitle = movieTitle.replace(/^[^a-zA-Z]*/, "")
-	var movieSimpleTitle = keyword1.replace(/第\S+季.*/, "")
-	var movieFinalTitle = MovieOriginalTitle.replace(/Season\s/, "S")
+  var configs = [{
+    enabled: true,
+    weight: 2,
+    src: 'https://btdb.eu/search/__keyword__/0/',
+    uiRef: function (self, data) {
+      var html = $.parseHTML(data.responseText);
+      var items = ''
+      var medias = $(html).find('.media')
+      medias.each(function () {
+        let {title, size, sd, dn, link} = self.fieldRef(this)
+        //items += `<li><a href="${link}">${title}</a><span style="float: right">sd: ${sd}, dn: ${dn}, ${size}</span></li>`
+        items += `<li><a href="${link}">${title} (sd: ${sd}, dn: ${dn}, ${size})</a></li>`
+      })
+      items = items.length > 0 ? items : '[btdb.eu] No any magnet links can be found!'
+      var layer = $(`<div class="clearfix" style="float: left; width: 675px"><hr/><ul>${items}</ul></div>`)
+      $('.article .subjectwrap:first').append(layer)
+    },
+    has: function (data) {
+      var html = $.parseHTML(data.responseText);
+      var items = ''
+      var medias = $(html).find('.media')
+      return medias.length > 0
+    },
+    fieldRef: function (html) {
+      var title = $(html).find('.media-body .item-title a').text()
+      var info = $(html).find('.media-body .item-meta-info small')
+      var link = $(html).find('.media-right a:first').attr('href')
+      var size = /Size\s:\s(.+)/g.exec($(info[0]).text())[1]
+      var sd = /Seeders\s:\s(\d*)/g.exec($(info[2]).text())[1]
+      var dn = /Leechers\s:\s(\d*)/g.exec($(info[3]).text())[1]
+      return {title: title, link: link, sd: sd, dn: dn, size: size}
+    }
+  }, {
+    enabled: true,
+    weight: 1,
+    src: 'http://kat.rip/usearch/__keyword__/',
+    uiRef: function (self, data) {
+      var html = $.parseHTML(data.responseText);
+      var items = ''
+      var medias = $(html).find('.mainpart .data .even, .odd')
+      medias.each(function () {
+        let {title, size, sd, dn, link} = self.fieldRef(this)
+        //items += `<li><a href="${link}">${title}</a><span style="float: right">sd: ${sd}, dn: ${dn}, ${size}</span></li>`
+        items += `<li><a href="${link}">${title} (sd: ${sd}, dn: ${dn}, ${size})</a></li>`
+      })
+      items = items.length > 0 ? items : '[kat.rip] No any magnet links can be found!'
+      var layer = $(`<div class="clearfix" style="float: left; width: 675px"><hr/><ul>${items}</ul></div>`)
+      $('.article .subjectwrap:first').append(layer)
+    },
+    has: function (data) {
+      var html = $.parseHTML(data.responseText);
+      var items = ''
+      var medias = $(html).find('.mainpart .data .even, .odd')
+      return medias.length > 0
+    },
+    fieldRef: function (html) {
+      var tds = $(html).find('td')
+      var title = tds.find('.torrentname .cellMainLink').text()
+      var link = tds.find('.floatright a:last').attr('href')
+      var size = $(tds[1]).text()
+      var sd = $(tds[4]).text()
+      var dn = $(tds[5]).text()
+      return {title: title, link: link, sd: sd, dn: dn, size: size}
+    }
+  }]
 
-  var url = encodeURI('https://btdb.eu/search/' + movieFinalTitle + '/0/')
+  var getKeyword = function () {
+    var txt = $('h1 span:eq(0)').text()
+    //txt = txt.replace(/^[^a-zA-Z]*/, "")
+    txt = txt.replace(/Season\s/, "S")
+    return txt
+  }
 
-  GM_xmlhttpRequest({
-       method: 'GET',
-       url: url,
-       onload: function(r) {
-           var data = r.responseText
-           var html = $.parseHTML(data);
-           var items = ''
-           var medias = $(html).find('.media')
-           medias.each(function(){
-               var title = $(this).find('.media-body .item-title a').text()
-               var size = $(this).find('.media-body .item-meta-info small:first').text()
-               var link = $(this).find('.media-right a:first').attr('href')
+  var doRequest = function (url) {
+    return new Promise(function (resolve, reject) {
+      GM_xmlhttpRequest({
+        method: 'GET',
+        url: url,
+        onabort: function (data) {
+          reject(data)
+        },
+        onerror: function (data) {
+          reject(data)
+        },
+        ontimeout: function (data) {
+          reject(data)
+        },
+        onload: function (data) {
+          resolve(data)
+        }
+      })
+    })
+  }
 
-               //layer.append(`<p><a href="${link}">${title}</a> - ${size}</p>`)
-               items += `<li><a href="${link}">${title}</a><span style="float: right">${size}</span></li>`
-           })
-           items = items.length > 0 ? items : 'No any magnet links can be found!'
-           var layer = $(`<div class="clearfix" style="float: left; width: 675px"><hr/><ul>${items}</ul></div>`)
-           $('.article .subjectwrap:first').append(layer)
-       }
-  })
+  Promise.each = async function (configs) {
+    configs.sort(function (a, b) {
+      return a.weight - b.weight
+    })
+
+    var keyword = getKeyword()
+    console.log('[btdouban] keyword: ', keyword)
+
+    for (var i = 0; i < configs.length; i++) {
+      var c = configs[i]
+      if (!c.enabled) continue
+
+      var url = c.src.replace('__keyword__', keyword)
+
+      url = encodeURI(url)
+      var data = await doRequest(url).catch(function (error) {
+        console.log(error)
+      })
+
+      c.uiRef(c, data)
+
+      if (c.has(data)) break
+    }
+  }
+
+  Promise.each(configs)
 })();
