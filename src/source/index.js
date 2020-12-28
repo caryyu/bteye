10d6b62017a3bb5aa46e58e0a9a690970dc821e8
Cary <caryyu.tg@gmail.com>
2020-12-28 21:40:24 +0800

    Get yts.mx supported (#3)
    * try bencode lib experiment
    
    * get yts.mx supported
    
    * get yts.mx supported
    
    * Get more torrents able to go
    
    * dist release

--- src/source/index.js
+++ src/source/index.js
@@ -2,11 +2,13 @@ const btdb_eu = require('./btdb_eu')
 const kat_rip = require('./kat_rip')
 const thepiratebay_org = require('./thepiratebay_org')
 const _1337x_to = require('./1337x_to')
+const yts_mx = require('./yts_mx')
 
 module.exports = [
   new btdb_eu(),
   new kat_rip(),
   new thepiratebay_org(),
-  new _1337x_to()
+  new _1337x_to(),
+  new yts_mx()
 ]
 
--- test/all.test.js
+++ test/all.test.js
@@ -3,6 +3,7 @@ const thepiratebay_org = require('../src/source/thepiratebay_org')
 const btdb_eu = require('../src/source/btdb_eu')
 const kat_rip = require('../src/source/kat_rip')
 const _1337x_to = require('../src/source/1337x_to')
+const yts_mx = require('../src/source/yts_mx')
 const expect = require('chai').expect
 
 describe('Do thepiratebay.org testing', function() {
@@ -37,6 +38,15 @@ describe('Do 1337x.to testing', function() {
     this.timeout(30000)
     var keyword = 'mulan'
     var result = await new _1337x_to().execute(keyword)
+    expect(result.length).to.be.greaterThan(0)
+  })
+})
+
+describe('Do yts.mx testing', function() {
+  it('the result size should be greater than 0', async function() {
+    this.timeout(30000)
+    var keyword = 'mulan'
+    var result = await new yts_mx().execute(keyword)
     expect(result.length).to.be.greaterThan(0)
   })
 })
