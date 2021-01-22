f96e764d2e03500a502ea14d8ffa28fc4838bb53
caryyu <cary.yu@wework.com>
2021-01-22 21:24:11 +0800

    can't pass through cloudflare-backed sites

--- src/source/index.js
+++ src/source/index.js
@@ -1,12 +1,16 @@
-const btdb_eu = require('./btdb_eu')
-const kat_rip = require('./kat_rip')
+// Note:
+// the ones being disabled are backed by Cloudflare which can't be passed through
+// @date 2021-01-22
+
+//const btdb_eu = require('./btdb_eu')
+//const kat_rip = require('./kat_rip')
 const thepiratebay_org = require('./thepiratebay_org')
 const _1337x_to = require('./1337x_to')
 const yts_mx = require('./yts_mx')
 
 module.exports = [
-  new btdb_eu(),
-  new kat_rip(),
+  //new btdb_eu(),
+  //new kat_rip(),
   new thepiratebay_org(),
   new _1337x_to(),
   new yts_mx()
--- test/all.test.js
+++ test/all.test.js
@@ -1,7 +1,7 @@
 const mocks = require('./mocks')
 const thepiratebay_org = require('../src/source/thepiratebay_org')
-const btdb_eu = require('../src/source/btdb_eu')
-const kat_rip = require('../src/source/kat_rip')
+//const btdb_eu = require('../src/source/btdb_eu')
+//const kat_rip = require('../src/source/kat_rip')
 const _1337x_to = require('../src/source/1337x_to')
 const yts_mx = require('../src/source/yts_mx')
 const expect = require('chai').expect
@@ -15,23 +15,23 @@ describe('Do thepiratebay.org testing', function() {
   })
 })
 
-describe('Do btdb.eu testing', function() {
-  it('the result size should be greater than 0', async function() {
-    this.timeout(30000)
-    var keyword = 'mulan'
-    var result = await new btdb_eu().execute(keyword)
-    expect(result.length).to.be.greaterThan(0)
-  })
-})
+//describe('Do btdb.eu testing', function() {
+  //it('the result size should be greater than 0', async function() {
+    //this.timeout(30000)
+    //var keyword = 'mulan'
+    //var result = await new btdb_eu().execute(keyword)
+    //expect(result.length).to.be.greaterThan(0)
+  //})
+//})
 
-describe('Do kat.rip testing', function() {
-  it('the result size should be greater than 0', async function() {
-    this.timeout(30000)
-    var keyword = 'mulan'
-    var result = await new kat_rip().execute(keyword)
-    expect(result.length).to.be.greaterThan(0)
-  })
-})
+//describe('Do kat.rip testing', function() {
+  //it('the result size should be greater than 0', async function() {
+    //this.timeout(30000)
+    //var keyword = 'mulan'
+    //var result = await new kat_rip().execute(keyword)
+    //expect(result.length).to.be.greaterThan(0)
+  //})
+//})
 
 describe('Do 1337x.to testing', function() {
   it('the result size should be greater than 0', async function() {
