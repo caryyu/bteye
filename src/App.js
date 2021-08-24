d0815e794010c9b388eeae3aa8603726f0191540
caryyu <caryyu.tg@gmail.com>
2021-08-24 21:55:56 +0800

    fix lib

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-export default function render() {
-  return (
-    <div className="clearfix magnet-section" style={style.section}>
-      <span>[bteye-btdouban] Loading Loading Loading ...</span>
-    </div>
-  )
+import React from 'react';
+const configs = require('./source/');
+
+export default class App extends React.Component {
+  async componentDidMount() {
+    var elements = []
+    for (var i = 0; i < configs.length; i++) {
+      var c = configs[i]
+      var s = c
+      
+      if (!s.enabled) continue
+
+      var k = c.filterKeyword(keyword)
+      if (k.length <= 0) continue
+
+      console.log(k)
+
+      var data = await s.execute(k).catch(function (error) {
+        console.log(error)
+        //var layer = $(`<div class="clearfix" style="float: left; width: 675px"><hr/><p>Seems like you're not using VPN</p></div>`)
+        //$('.article .subjectwrap:first').append(layer)
+      })
+
+      //var items = ''
+
+      if (!data || data.length <= 0) {
+        //items = `[${s.site}] No any magnet links can be found!`
+        continue
+      } else {
+        data.forEach(item => {
+          let {title, size, sd, lc, link} = item
+          //items += `<li><a href="javascript:void(0)" link="${link}">[Play]</a> <a href="${link}">${title} (sd: ${sd}, lc: ${lc}, ${size})</a></li>`
+          //items += `<li>[${s.site}] <a href="${link}">${title} (sd: ${sd}, lc: ${lc}, ${size})</a></li>`
+          elements.push($(`<li>[${s.site}] <a href="${link}">${title} (sd: ${sd}, lc: ${lc}, ${size})</a></li>`))
+        })
+        // loop break
+        // i = configs.length - 1
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="clearfix magnet-section" style={style.section}>
+        <span>[bteye-btdouban] Loading Loading Loading ...</span>
+      </div>
+    )
+  }
 }
 
 const style = {
